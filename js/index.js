2c483e95648bb0d9be542db826eabeefe5a366b4
filecc <fil.gia@live.it>
2023-03-24 15:51:42 +0100

    added no result

--- js/index.js
+++ js/index.js
@@ -1,231 +1,245 @@
-/* Add the specified classes to the given HTML element */
-function addClasses(element, classes) {
-  classes.forEach((_class) => element.classList.add(_class));
-}
+(function () {
+  /* Add the specified classes to the given HTML element */
+  function addClasses(element, classes) {
+    classes.forEach((_class) => element.classList.add(_class));
+  }
 
-/* Adds the specified ID to the given HTML element */
-function addID(element, id) {
-  element.setAttribute('id', id);
-}
+  /* Adds the specified ID to the given HTML element */
+  function addID(element, id) {
+    element.setAttribute("id", id);
+  }
 
-/* Creates a new HTML element with an object in input */
-function createChild(tag, props = {}) {
-  const newElement = document.createElement(tag);
+  /* Creates a new HTML element with an object in input */
+  function createChild(tag, props = {}) {
+    const newElement = document.createElement(tag);
 
-  Object.entries(props).forEach(([key, value]) => {
-    switch (key) {
-      case 'id':
-        addID(newElement, value);
-        break;
-      case 'classes':
-        addClasses(newElement, value);
-        break;
-      case 'text':
-        newElement.textContent = value;
-        break;
-      default:
-        newElement.setAttribute(key, value);
-        break;
+    Object.entries(props).forEach(([key, value]) => {
+      switch (key) {
+        case "id":
+          addID(newElement, value);
+          break;
+        case "classes":
+          addClasses(newElement, value);
+          break;
+        case "text":
+          newElement.textContent = value;
+          break;
+        default:
+          newElement.setAttribute(key, value);
+          break;
+      }
+    });
+    return newElement;
+  }
+
+  /* Remove the specified classes to the given HTML element */
+  function removeClasses(element, classes) {
+    classes.forEach((_class) => element.classList.remove(_class));
+  }
+
+  /* Toggle the specified classes to the given HTML element */
+  function toggleClasses(element, classes) {
+    classes.forEach((_class) => element.classList.toggle(_class));
+  }
+
+  /* Adds the desired attribute to the given element */
+  function addAttributes(element, props) {
+    const { ...rest } = props;
+    Object.entries(rest).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  }
+
+  // function to get a random number, extreme included
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  const icons = [
+    {
+      name: "cat",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "crow",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "dog",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "dove",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "dragon",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "horse",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "hippo",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "fish",
+      prefix: "fa-",
+      type: "animal",
+      family: "solid",
+      color: "orange",
+    },
+    {
+      name: "carrot",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "solid",
+      color: "green",
+    },
+    {
+      name: "apple-alt",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "solid",
+      color: "green",
+    },
+    {
+      name: "lemon",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "solid",
+      color: "green",
+    },
+    {
+      name: "pepper-hot",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "solid",
+      color: "green",
+    },
+    {
+      name: "user-astronaut",
+      prefix: "fa-",
+      type: "user",
+      family: "solid",
+      color: "blue",
+    },
+    {
+      name: "user-graduate",
+      prefix: "fa-",
+      type: "user",
+      family: "solid",
+      color: "blue",
+    },
+    {
+      name: "user-ninja",
+      prefix: "fa-",
+      type: "user",
+      family: "solid",
+      color: "blue",
+    },
+    {
+      name: "user-secret",
+      prefix: "fa-",
+      type: "user",
+      family: "solid",
+      color: "blue",
+    },
+  ];
+
+  function createCol(icon) {
+    const { name, prefix, type, family, color } = icon;
+
+    const col = createChild("div", {
+      classes: ["col-12", "p-4"],
+      "data-fg-type": type,
+    });
+    const card = createChild("div", { classes: ["cardIcon"] });
+    col.appendChild(card);
+    const iconImage = createChild("i", {
+      classes: [`${prefix}${family}`, `${prefix}${name}`],
+    });
+    const iconName = createChild("p", { text: name });
+    card.append(iconImage, iconName);
+    iconImage.style.color = color;
+
+    return col;
+  }
+
+  const root = document.getElementById("root");
+  const select = document.getElementById("typeSelector");
+
+  const typeSelectable = [];
+
+  icons.map((icon, index) => {
+    if (index === 0) {
+      typeSelectable.push(icon.type);
+    } else if (!typeSelectable.includes(icon.type)) {
+      typeSelectable.push(icon.type);
     }
   });
-  return newElement;
-}
 
-/* Remove the specified classes to the given HTML element */
-function removeClasses(element, classes) {
-  classes.forEach((_class) => element.classList.remove(_class));
-}
-
-/* Toggle the specified classes to the given HTML element */
-function toggleClasses(element, classes) {
-  classes.forEach((_class) => element.classList.toggle(_class));
-}
-
-/* Adds the desired attribute to the given element */
-function addAttributes(element, props) {
-  const { ...rest } = props;
-  Object.entries(rest).forEach(([key, value]) => {
-    element.setAttribute(key, value);
+  typeSelectable.forEach((element) => {
+    select.appendChild(
+      createChild("option", { text: element, value: element })
+    );
   });
-}
 
-// function to get a random number, extreme included
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-
-const icons = [
-  {
-      name: 'cat',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'crow',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'dog',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'dove',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'dragon',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'horse',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'hippo',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'fish',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'solid',
-      color: 'orange'
-  },
-  {
-      name: 'carrot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'solid',
-      color: 'green'
-  },
-  {
-      name: 'apple-alt',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'solid',
-      color: 'green'
-  },
-  {
-      name: 'lemon',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'solid',
-      color: 'green'
-  },
-  {
-      name: 'pepper-hot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'solid',
-      color: 'green'
-  },
-  {
-      name: 'user-astronaut',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'solid',
-      color: 'blue'
-  },
-  {
-      name: 'user-graduate',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'solid',
-      color: 'blue'
-  },
-  {
-      name: 'user-ninja',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'solid',
-      color: 'blue'
-  },
-  {
-      name: 'user-secret',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'solid',
-      color: 'blue'
-  }
-];
-
-
-function createCol(icon){
-  const {name, prefix, type, family, color} = icon;
-
-  const col = createChild('div', {classes: ['col-12', 'p-4'], 'data-fg-type': type});
-  const card = createChild('div', {classes: ['cardIcon']});
-  col.appendChild(card);
-  const iconImage = createChild('i', {classes: [`${prefix}${family}`, `${prefix}${name}`]});
-  const iconName =  createChild('p', {text: name});
-  card.append(iconImage, iconName);
-  iconImage.style.color = color;
-
-  return col
-}
-
-const root = document.getElementById('root');
-const select = document.getElementById('typeSelector');
-
-const typeSelectable = [];
-
-icons.map((icon, index) => {
-  if (index === 0) {
-    typeSelectable.push(icon.type);
-  } else if (!typeSelectable.includes(icon.type)){
-    typeSelectable.push(icon.type)
-  }
-})
-
-typeSelectable.forEach(element => {
-  select.appendChild(createChild('option', {text: element, value: element}));
-});
-
-
-icons.forEach(icon => {
-  root.appendChild(createCol(icon));
-});
-
-function search(value){
-  console.log(value)
-  const searched = icons.filter(icon => ((icon.type).includes(value.toLowerCase()) || (icon.name).includes(value.toLowerCase())));
-  root.textContent = '';
-  const setToDisplay = (value === 'all' || value === '') ? icons : searched;
-  setToDisplay.forEach(icon => {
+  icons.forEach((icon) => {
     root.appendChild(createCol(icon));
   });
-}
 
+  function search(value) {
+    const searched = icons.filter(
+      (icon) =>
+        icon.type.includes(value.toLowerCase()) ||
+        icon.name.includes(value.toLowerCase())
+    );
+    root.textContent = "";
+    const setToDisplay = value === "all" || value === "" ? icons : searched;
+    if (searched.length === 0) {
+      root.appendChild(createChild('span', {classes: ['text-danger', 'text-center'], text: 'No result'}));
+    } else {
+      setToDisplay.forEach((icon) => {
+        root.appendChild(createCol(icon));
+      });
+    }
+    
+  }
 
-select.addEventListener('change', function() {
-  const indexSelected = this.selectedIndex;
-  const opstionSelected = select.options[indexSelected].value;
-  search(opstionSelected);  
-});
+  select.addEventListener("change", function () {
+    const indexSelected = this.selectedIndex;
+    const opstionSelected = select.options[indexSelected].value;
+    search(opstionSelected);
+  });
 
-const input = document.querySelector('input');
-input.addEventListener('input', function() {
-  const value = this.value;
-  search(value)
-})
+  const input = document.querySelector("input");
+  input.addEventListener("input", function () {
+    const value = this.value;
+    search(value);
+  });
+  
+})();
